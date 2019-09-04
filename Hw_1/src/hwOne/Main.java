bfd258a522ccb41c4ca1feb72df77198a05e4605
jsmith264 <jsmith264@OVDIMC10-41V.coast.cccd.adx>
2019-09-04 12:37:27 -0700

    asdf

---
+++ Hw_1/src/hwOne/Main.java
@@ -0,0 +1,14 @@
+package hwOne;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("Hi");
+        
+
+    }
+
+}
